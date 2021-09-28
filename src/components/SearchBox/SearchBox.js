476d9a1a06fb1819b5ec4c5c09d58be8e0ddc08c
FAIDINE <mohamed.faidine@live.fr>
2021-09-28 11:07:34 +0200

    refact(SearchBox) change background and add responsive

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import Classes from './SearchBox.module.css';
-import ImgSearchBox from '../../assets/images/Bouillouse.png';
 
 const searchBox = (props) => (
-    <div className={["container p-2", Classes.searchB].join(' ')}>
-        <img src={ImgSearchBox} alt="imgHome" />
-        <div className={[Classes.formDiv].join( '')}>
-            <form className={["p-2", Classes.formSearch].join(' ')}>
-                <input className="p-4" type="search" placeholder="Où voulez-vous allez ?" id="search" />
-            </form>
-        </div>
+    <div className={[Classes.formDiv].join( '')}>
+        <form className={["p-2", Classes.formSearch].join(' ')}>
+            <input className="p-4" type="search" placeholder="Où voulez-vous allez ?" id="search" />
+        </form>
     </div>
 );
 
